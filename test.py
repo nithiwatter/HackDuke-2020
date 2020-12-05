4aa7a243be72d8efc54213ea6f1da5869b15d8e2
Isaac Poarch <ipoarch1@gmail.com>
2020-12-05 15:56:29 -0500

    test

--- test.py
+++ test.py
@@ -20,25 +20,31 @@ def get_aq(lat, long):
 def get_other_shit(lat, long):
     return 5
 
-def cost(lats_longs, funcs, weights, km_per=1):
+def cost(lats_longs, funcs, weights, km_per=10):
     costs = np.empty([len(lats_longs), len(funcs)])
     index = 0
     for lat_long in lats_longs:
         polsum = np.zeros(len(funcs))
         for i in range(len(lat_long) - 1):
-            distance = haversine((radians(lat_long[i][0]), radians(lat_long[i][1])), (radians(lat_long[i+1][0]), radians(lat_long[i+1][1])))
+            distance = haversine((lat_long[i][0], lat_long[i][1]), (lat_long[i+1][0], lat_long[i+1][1]))
             pts = int(np.floor(distance / km_per))
             if pts < 2: pts = 2
             lats = np.linspace(lat_long[i][0], lat_long[i][0], pts)
             longs = np.linspace(lat_long[i][1], lat_long[i][1], pts)
             polsum_local = np.zeros(len(funcs))
+            last_add = 0
             for j in range(len(lats)):
                 added_v = np.empty([len(funcs)])
                 for f in range(len(funcs)):
-                    added_v[f] = funcs[f](lats[j], longs[j]) * distance / pts
+                    new_add = funcs[f](lats[j], longs[j])
+                    if new_add == -1:
+                        added_v[f] = last_add
+                    else:
+                        added_v[f] = new_add * distance / pts
+                    last_add = added_v[f]
                     if j == 0 or j == len(lats) - 1: added_v[f] = added_v[f] / 2.0
                 polsum_local = polsum_local + added_v
-                sleep(0.1)
+                sleep(0.11)
             polsum = polsum + polsum_local
         costs[index] = polsum
         index = index + 1
