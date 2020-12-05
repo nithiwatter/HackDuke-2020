8cdc59f178d11f17aba22dc2e184c629c770bc92
harryxu2626 <43221425+harryxu2626@users.noreply.github.com>
2020-12-05 11:39:26 -0500

    created .env and added iqair api key

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 from flask import Flask, jsonify
+from dotenv import load_dotenv
+import os
 
 app = Flask(__name__)
+load_dotenv()
+
+
+IQAIR_API_KEY = os.getenv("IQAIR_API_KEY")
 
 @app.route('/')
 def index():
+    print(IQAIR_API_KEY)
     return 'index'
 
 
