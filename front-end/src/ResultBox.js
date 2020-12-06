90de41eaa4bd8716b5504d095e4d13f519657a86
Nithiwat Seesillapachai <nithiwatter@gmail.com>
2020-12-06 22:58:44 +0700

    result box

--- front-end/src/ResultBox.js
+++ front-end/src/ResultBox.js
@@ -1,4 +1,5 @@
 import React from "react";
+import axios from "axios";
 import { Button, Typography, makeStyles } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
@@ -7,12 +8,13 @@ const useStyles = makeStyles((theme) => ({
 
 export default function ResultBox() {
   const classes = useStyles();
+
   return (
     <div className={classes.root}>
       <Typography variant="h5" align="center">
         Friendship Tracker
       </Typography>
-      <Button>Click</Button>
+      <Button variant="contained">Click</Button>
     </div>
   );
 }
